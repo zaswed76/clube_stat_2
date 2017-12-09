66ab700d20f9e38bdd4a766593758ef525c7ec4b
Cassa <zaswed76@gmail.com>
2017-12-09 09:53:53 +0200

    create_project

--- clubestat/sched/t1.py
+++ clubestat/sched/t1.py
@@ -1,15 +1,21 @@
 
-from datetime import date, datetime
-
+from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+import logging
+
+logging.basicConfig(
+    format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+    level=logging.WARNING,
+    filename=u'mylog4.log')
+
+def job_function():
+    print(str(datetime.now()))
+    logging.warning(u'info')
+    logging.error(u'This is an info message')
 
 sched = BlockingScheduler()
+sched.add_job(job_function, 'interval', minutes=5, start_date="2017-12-9 09:00:00")
 
-def my_job(text):
-    print(text)
-
-# The job will be executed on November 6th, 2009
-sched.add_job(my_job, 'date', run_date=datetime(2017, 12, 8, 20, 45, 0), args=['text'])
-
+logging.warning(u'старт')
 sched.start()
