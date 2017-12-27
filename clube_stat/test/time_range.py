08fecc641aa19f1e428fd2aa8104459f3c1a3ba1
Cassa <zaswed76@gmail.com>
2017-12-27 20:38:56 +0200

    create_project

--- clube_stat/test/time_range.py
+++ clube_stat/test/time_range.py
@@ -1,7 +1,12 @@
-import arrow
-from datetime import datetime
+# import arrow
+#
+# start = arrow.Arrow(2017, 12, 27, 9, 0)
+# end = arrow.Arrow(2017, 12, 30, 9, 0)
+# for r in arrow.Arrow.range('hour', start, end):
+#      print(r.hour)
 
-start = datetime(2017, 12, 27, 9, 0)
-end = datetime(2017, 12, 30, 9, 0)
-for r in arrow.Arrow.span_range('hour', start, end):
-     print(r[0].hour)
+import prettytable
+
+table = prettytable.PrettyTable(["x", "y"])
+table.add_row([1, 2])
+print(table)
