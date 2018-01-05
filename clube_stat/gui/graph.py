6d2b28cbc7a93c0ca0bceca35372456506a499e1
serg <zaswed76@gmail.com>
2018-01-05 21:36:40 +0200

    test1

--- clube_stat/gui/graph.py
+++ clube_stat/gui/graph.py
@@ -10,15 +10,10 @@ root = os.path.join(os.path.dirname(__file__))
 ui_pth = os.path.join(root, "ui/graph_form.ui")
 
 class GraphicsWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        # self.clubs_object = clubs_object
-        # self.pxm = None
-        # self.current_club_name = None
-        # self.db_path = db_path
-        # self.resize(500, 500)
+    def __init__(self,  *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.form = uic.loadUi(ui_pth, self)
-        # self.setWindowTitle("Graphics")
+        self.setWindowTitle("Graphics")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
