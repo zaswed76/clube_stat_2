1127209c6dd2ca3b7df21e50a820f6086cb9c1b1
Admin <zaswed76@gmail.com>
2018-02-18 22:02:36 +0200

    run not pwd

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         install_requires=[],
         entry_points={
             'console_scripts':
-                ['stat4 = clube_stat.main:main']
+                ['stat8 = clube_stat.main:main']
         }
 
 )
